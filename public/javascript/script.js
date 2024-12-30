94f456f0c736591479c485b6cbc41a2fc039eb81
RiCo-OneilL <oneillrietchiey@gmail.com>
2024-12-30 18:13:53 +0300

    Working password view buttons

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -4,6 +4,7 @@ const pwFields = document.querySelectorAll(".password");
 const signUp = document.querySelector(".signup-link");
 const login = document.querySelector(".login-link");
 
+/*
 // js code to show/hide password and change icon
 pwShowHide.forEach((eyeIcon) => {
   eyeIcon.addEventListener("click", () => {
@@ -22,5 +23,20 @@ pwShowHide.forEach((eyeIcon) => {
         });
       }
     });
+  });
+});
+*/
+
+// Show/hide password and change icon
+pwShowHide.forEach((eyeIcon, index) => {
+  eyeIcon.addEventListener("click", () => {
+    const pwField = pwFields[index]; // Match the icon to its respective password field
+    if (pwField.type === "password") {
+      pwField.type = "text";
+      eyeIcon.classList.replace("uil-eye-slash", "uil-eye"); // Update icon
+    } else {
+      pwField.type = "password";
+      eyeIcon.classList.replace("uil-eye", "uil-eye-slash"); // Revert icon
+    }
   });
 });
